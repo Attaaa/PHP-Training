3916318cd97b8ba7886360304f132811c39a3e60
Attaaa <hatta.eka.p@gmail.com>
2018-02-11 05:41:49 -0800

    New index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Training</title>
+</head>
+<body>
+    <section>
+        <img src="images/mail-flat.png" alt="mailto">
+        <form>
+            <table>
+                <tr>
+                    <td>Full Name</td>
+                    <td>:</td>
+                    <td><input type="username" name="fname"></td>
+                </tr>
+                <tr>
+                    <td>Username</td>
+                    <td>:</td>
+                    <td><input type="username" name="uname"></td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>:</td>
+                    <td><input type="email" name="mail"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>:</td>
+                    <td><input type="password" name="psword"></td>
+                </tr>
+            </table>
+        </form>
+    </section>
+</body>
+</html>
