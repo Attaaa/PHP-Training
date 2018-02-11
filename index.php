7dfab6b4e9ada445e30f8d8981f8e75848421690
Attaaa <hatta.eka.p@gmail.com>
2018-02-11 06:18:42 -0800

    adding style

--- index.php
+++ index.php
@@ -5,31 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Training</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <section>
-        <img src="images/mail-flat.png" alt="mailto">
         <form>
+            <center><img class="logo" src="images/mail-flat.png" alt="mailto"><center>
             <table>
                 <tr>
                     <td>Full Name</td>
-                    <td>:</td>
+                    <td>: </td>
                     <td><input type="username" name="fname"></td>
                 </tr>
                 <tr>
                     <td>Username</td>
-                    <td>:</td>
+                    <td>: </td>
                     <td><input type="username" name="uname"></td>
                 </tr>
                 <tr>
                     <td>Email</td>
-                    <td>:</td>
+                    <td>: </td>
                     <td><input type="email" name="mail"></td>
                 </tr>
                 <tr>
                     <td>Password</td>
-                    <td>:</td>
+                    <td>: </td>
                     <td><input type="password" name="psword"></td>
+                </tr>
+                <tr>
+                    <td class="last" colspan="3">
+                        <center><a class="btn" href="">Register</a></center>
+                    </td>
                 </tr>
             </table>
         </form>
